2a9eca1988f563ba796f0b978f4fb38dc7bd62ce
chas <123321721+chassinat0r@users.noreply.github.com>
2025-04-15 18:56:07 +0100

    remove builds

--- build/main.d
+++
@@ -1,9 +0,0 @@
-main.o: /home/chas/Documents/Projects/CG50/Dodger/src/main.c \
- /home/chas/PrizmSDK/include/fxcg/display.h \
- /home/chas/PrizmSDK/include/fxcg/keyboard.h \
- /home/chas/Documents/Projects/CG50/Dodger/include/player.h \
- /home/chas/Documents/Projects/CG50/Dodger/include/constants.h
-/home/chas/PrizmSDK/include/fxcg/display.h:
-/home/chas/PrizmSDK/include/fxcg/keyboard.h:
-/home/chas/Documents/Projects/CG50/Dodger/include/player.h:
-/home/chas/Documents/Projects/CG50/Dodger/include/constants.h:
--- build/player.d
+++
@@ -1,9 +0,0 @@
-player.o: /home/chas/Documents/Projects/CG50/Dodger/src/player.c \
- /home/chas/Documents/Projects/CG50/Dodger/include/player.h \
- /home/chas/Documents/Projects/CG50/Dodger/include/util.h \
- /home/chas/Documents/Projects/CG50/Dodger/include/constants.h \
- /home/chas/PrizmSDK/include/fxcg/display.h
-/home/chas/Documents/Projects/CG50/Dodger/include/player.h:
-/home/chas/Documents/Projects/CG50/Dodger/include/util.h:
-/home/chas/Documents/Projects/CG50/Dodger/include/constants.h:
-/home/chas/PrizmSDK/include/fxcg/display.h:
